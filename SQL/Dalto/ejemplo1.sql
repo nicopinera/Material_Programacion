d098a93c411dd2b539bfdce04e9e6daf55819302
Nicolas Piñera <nicolaspinera@gmail.com>
2025-10-19 23:00:46 -0300

    Added table creation and improved comments in ejemplo1.sql

--- SQL/Dalto/ejemplo1.sql
+++ SQL/Dalto/ejemplo1.sql
@@ -1,6 +1,13 @@
+CREATE "users"(
+    Nombre TEXT,
+    Apellido TEXT,
+    Edad INTEGER
+)
+/* Utilizando bases de datos*/
 
-
+/* INSERT SIRVE PARA GUARDAD VALORES EN UNA TABLA ESPECIFICA*/
 INSERT INTO users (Nombre,Apellido,Edad) VALUES ('Nico','Piñera',22),('Lucas','Dalto',23);
 
+/* EL SELECT ME SIRVE PARA SELECCIONAR CAMPOS ESPECIFICOS D UNA TABLA*/
 SELECT * FROM users;
 
